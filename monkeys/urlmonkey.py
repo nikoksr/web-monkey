c6fe7bb4a0cc72a045bfbcfbfda40540dcb8aba3
Niko Köser <koeserniko@gmail.com>
2018-09-24 17:00:51 +0200

    Improve website identification

--- monkeys/urlmonkey.py
+++ monkeys/urlmonkey.py
@@ -1,7 +1,8 @@
 """ imports """
 import argparse
-from bs4 import BeautifulSoup
+
 import requests
+from bs4 import BeautifulSoup
 
 
 class UrlMonkey():
@@ -21,31 +22,64 @@ class UrlMonkey():
         """ Inits urlmonkey class with an empty tree list """
         self.known_trees = []
 
-    def find_trees(self, root=None):
+    def get_tree_root(self, tree=None):
+        """ 
+        Returns the root of a tree (main-url of a sub-url) 
+        e.g.:   tree: https://www.python.org/about/
+                root: https://www.python.org/
+        """
+
+        third_slash = 0
+        counter = 0
+
+        for i in range(len(tree)):
+            if (tree[i] == '/'):
+                counter += 1
+                if (counter == 3):
+                    third_slash = i
+
+        root = tree[:third_slash + 1]
+
+        return root
+
+    def find_trees(self, tree=None):
         """ Searches for new trees starting from the root """
-        if root is None:
+        if tree is None:
             print('Invalid URL')
             return
 
-        try:
-            self.known_trees.append(root)
+        if (not tree.endswith('/')):
+            tree += '/'
 
+        self.known_trees.append(tree)
+
+        try:
             for known_tree in self.known_trees:
                 print('> ' + known_tree)
 
+                root = self.get_tree_root(known_tree)
+
                 search_branches = requests.get(known_tree)
-                branches = BeautifulSoup(
-                    search_branches.content, 'html.parser')
+                branches = BeautifulSoup(search_branches.content,
+                                         'html.parser')
                 branches = branches.find_all('a')
 
-                if branches is None:
+                if (branches is None):
                     continue
 
                 for branch in branches:
                     branch = branch.get('href')
 
-                    if (branch is None or branch[0:4] != 'http'
-                            or branch in self.known_trees):
+                    if (branch is None):
+                        continue
+
+                    if (branch[0:4] != 'http'):
+                        while (branch.startswith('/')):
+                            branch = branch[1:]
+
+                        branch = root + branch
+
+                    if (branch in self.known_trees):
                         continue
 
                     self.known_trees.append(branch)
@@ -58,7 +92,8 @@ if __name__ == '__main__':
     PARSER = argparse.ArgumentParser()
     PARSER.add_argument(
         "url",
-        help="Enter a valid url to start the infinite search for all branches " "and trees surrounding it")
+        help="Enter a valid url to start the infinite search for all branches "
+        "and trees surrounding it")
 
     ARGS = PARSER.parse_args()
     DONKEY_KONG = UrlMonkey()
